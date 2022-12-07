904ea8aab89629bb5c5c33cd174c6c47f729bce0
Hazael Lopez <elhazael134@gmail.com>
2022-12-07 16:39:34 -0600

    GifExpertApp - Component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,11 @@
 export const GifExpertApp = () => {
   return (
     <>
+        {/* titulo */}
         <h1>Gif Expert App</h1>
+        {/* input */}
+        {/* Estado de Gif */}
+        {/* Gif Item */}
     </>
   )
 }
