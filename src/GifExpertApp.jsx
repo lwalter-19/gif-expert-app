c249918e739ad07f6829e9baebaa862d3449c00d
Hazael Lopez <elhazael134@gmail.com>
2022-12-07 16:49:22 -0600

    Creando una lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        {/* titulo */}
-        <h1>Gif Expert App</h1>
-        {/* input */}
-        {/* Estado de Gif */}
-        {/* Gif Item */}
-    </>
-  )
+
+    const [categories, setCategories] = useState(['One Punch', 'Naruto', 'Boruto', 'Kimetsu']);
+
+    return (
+        <>
+            {/* titulo */}
+            <h1>Gif Expert App</h1>
+            {/* input */}
+            {/* Estado de Gif */}
+            <ol>
+                {
+                    categories.map(category => <li key={category}>{category}</li>)
+                }
+            </ol>
+            {/* Gif Item */}
+        </>
+    )
 }
