a864a1151e5b02ce70c1676e3fe9046d066181c6
Hazael Lopez <elhazael134@gmail.com>
2022-12-08 09:37:45 -0600

    Demostración de producción rápido

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { GifExpertApp } from './GifExpertApp'
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.Fragment>
+  <React.StrictMode>
     <GifExpertApp />
-  </React.Fragment>
+  </React.StrictMode>
 )
