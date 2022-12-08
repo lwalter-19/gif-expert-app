974cab12492da052c82b8def25a129f617ee390e
Hazael Lopez <elhazael134@gmail.com>
2022-12-07 21:28:18 -0600

    Fetch API - Obtener las imágenes deseadas getGifs.js

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,17 +1,5 @@
+import { getGifs } from "../helpers";
 
-const getGifs = async (category) => {
-    const api_key = 'oUY8Afbniwi2jDQu8Uwzx5O8pl1Te2xL';
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=20`;
-
-    const resp = await fetch(url);
-    const {data} = await resp.json();
-    const gifs = data.map(img => ({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url
-    }));
-    return gifs;
-}
 
 export const GifGrid = ({category}) => {
   
